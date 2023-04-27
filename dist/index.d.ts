c1a938edfb5a19fda476ab5b5f86d05b74a6eff5
Jeremy Wells <jemmyw@gmail.com>
2023-04-28 10:43:29 +1200

    ADT-516 Remove dist folder from git and bump version

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { ApplicationController, StartControllerScope, ControlledComponent, useController } from './controller/ApplicationController';
-import { raw, observe, unobserve } from '@nx-js/observer-util';
-import { randomId } from './utils/randomId';
-import type { ComponentType } from 'react';
-declare function ApplicationView<T extends ComponentType>(component: T): T;
-export default ApplicationController;
-export { ApplicationController, StartControllerScope, ControlledComponent, useController, ApplicationView, raw, observe, unobserve, randomId, };
