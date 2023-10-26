ea908ee20b16361c173236bd9742619248c26b63
Jeremy Wells <jemmyw@gmail.com>
2023-10-27 11:42:22 +1300

    Add license information for view()

--- src/ApplicationView.ts
+++ src/ApplicationView.ts
@@ -1,3 +1,29 @@
+/**
+ * This is an updated version of the original react-easy-state view function
+ */
+
+// MIT License
+//
+// Copyright (c) 2019 Miklos Bertalan
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import { observe, unobserve } from '@nx-js/observer-util';
 import React, { memo, useEffect, useMemo, useState } from 'react';
 
@@ -127,6 +153,10 @@ interface Options {
   debugger?: Function;
 }
 
+/**
+ * Updates in the given callback are batched together and only trigger a single
+ * render.
+ */
 export async function batch<T>(fn: () => Promise<T>): Promise<T> {
   ViewUpdateEmitter.updateAll();
 
@@ -136,6 +166,14 @@ export async function batch<T>(fn: () => Promise<T>): Promise<T> {
   return result;
 }
 
+/**
+ * Updates in the given callback are executed immediately and trigger a render
+ * immediately rather than waiting for the next microtask. This is useful for
+ * animation tasks.
+ * 
+ * This can have a significant performance cost so should only be used if
+ * required.
+ */
 export function immediate<T>(fn: () => T): T {
   ViewUpdateEmitter.immediate = true;
   const result = fn();
